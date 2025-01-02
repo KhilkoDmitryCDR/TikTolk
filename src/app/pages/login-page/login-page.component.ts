baf5ba3952c4bb04744d9e6aee2c7b78bc7000cf
Dmitry Khilko <dmitry.jilko@gmail.com>
2025-01-02 20:40:36 +0100

    add auth service

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService:AuthService=inject(AuthService)
+  form: FormGroup<{username: FormControl, password:FormControl}>= new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
+
+  onSubmit(){
+    if(this.form.value){
+
+      //@ts-ignore
+    this.authService.login(this.form.value)
+
+    }
+
+  }
 
 }
